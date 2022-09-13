a23dac86571afd97a467fc26a626450ad9b31f69
Anna <filippovaanna8888@gmail.com>
2022-09-13 23:00:43 +0300

    456

--- dz7/Program.cs
+++ dz7/Program.cs
@@ -78,25 +78,25 @@
 // 5 9 2 3
 // 8 4 2 4
 // 
-Random random = new Ramdom;
-int[,] matrix = new int [random.Next(1,10), random.Next(1,10)];
-for (int i=0; i< matrix.GetLength(0); i++)
-{
-    for(int j=0;j< matrix.GetLength(1); j++)
-    {
-        matrix [i;j]=random.Next(1,10);
-        Console.Write(matrix [i;j]+"");
-    }
-    Console.WriteLine();
+// Random random = new Ramdom;
+// int[,] matrix = new int [random.Next(1,10), random.Next(1,10)];
+// for (int i=0; i< matrix.GetLength(0); i++)
+// {
+//     for(int j=0;j< matrix.GetLength(1); j++)
+//     {
+//         matrix [i;j]=random.Next(1,10);
+//         Console.Write(matrix [i;j]+"");
+//     }
+//     Console.WriteLine();
 
-}
-Console.WriteLine("----------------------");
-for(int j=0;j< matrix.GetLength(1);j++)
-{
-    double sum =0;
-    for (int i=0; i< matrix.GetLength(0); i++)
-    {
-        sum+= matrix [i;j];
-    }
-    Console.Write($"{Math.Round(sum/matrix.GetLength(0)2)}")});
-}
+// }
+// Console.WriteLine("----------------------");
+// for(int j=0;j< matrix.GetLength(1);j++)
+// {
+//     double sum =0;
+//     for (int i=0; i< matrix.GetLength(0); i++)
+//     {
+//         sum+= matrix [i;j];
+//     }
+//     Console.Write($"{Math.Round(sum/matrix.GetLength(0)2)}")});
+// }
